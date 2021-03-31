18d0137a61adc436911222f3169d4a1c6b415aa4
Leandro Donizete <leandrosantos266@gmail.com>
2021-03-31 15:41:00 -0300

    aula inicial javascript

---
+++ js/main.js
@@ -0,0 +1,115 @@
+
+/*
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/vemcodoni");
+    //window.location.href = "https://github.com/vemcodoni";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert ("trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+function validaIdade(idade){
+    var validar
+    if (idade>=18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+   return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+var d = new Date();
+
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+var count;
+for(count=0; count<=5; count++){
+    alert(count);
+}
+
+
+
+
+var count = 0;
+while (count<= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+}
+
+
+var idade = prompt("Qual sua idade?");
+if (idade>= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+
+
+
+
+
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+
+
+
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" "));
+
+
+
+
+
+var nome = "Leandro Donizete";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
+
